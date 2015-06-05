6a86697742361fee61db77136a8329a0e8d7baa1
sukitha <sukithaj@gmail.com>
2015-06-05 15:36:36 +0530

    add basic service methods

---
+++ AutoAttendantServer.js
@@ -0,0 +1,37 @@
+var dbmodel = require('DVP-DBModels');
+var logger = require('DVP-Common/LogHandler/CommonLogHandler.js').logger;
+
+
+function GetAutoAttendant(extension, company, tenant, CallBack){
+
+
+    logger.debug("DVP-AutoAttendant.GetAutoAttendant Internal method ");
+
+
+    dbmodel.AutoAttendant.find({where: [{Extention: extension, Company: company, Tenant : tenant}]}).complete(function (err, aaData) {
+
+
+        if (!err && aaData) {
+
+            logger.debug("Got Auto attendant data");
+
+            try {
+
+                CallBack(aaData);
+
+            } catch(exp) {
+
+            }
+
+        } else {
+
+
+
+        }
+
+    })
+
+
+}
+
+module.exports.GetAutoAttendant = GetAutoAttendant;
