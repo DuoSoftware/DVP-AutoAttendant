e671f3ae2af2637c90b3e321ab0b471ae0227378
sukitha <sukithaj@gmail.com>
2015-07-30 16:44:09 +0530

    package.json added

---
+++ Config/custom-environment-variables.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "DB": {
+        "Type":"SYS_DATABASE_TYPE",
+        "User":"SYS_DATABASE_POSTGRES_USER",
+        "Password":"SYS_DATABASE_POSTGRES_PASSWORD",
+        "Port":"SYS_SQL_PORT",
+        "Host":"SYS_DATABASE_HOST",
+        "Database":"SYS_DATABASE_POSTGRES_USER"
+    },
+
+
+    "Redis":
+    {
+        "ip": "SYS_REDIS_HOST",
+        "port": "SYS_REDIS_PORT"
+
+    },
+
+    "Host":
+    {
+        "vdomain": "VIRTUAL_HOST",
+        "domain": "HOST_NAME",
+        "port": "HOST_CLUSTER_PORT",
+        "version": "HOST_VERSION"
+    },
+
+    "LBServer" : {
+
+        "ip": "LB_IP",
+        "port": "LB_PORT"
+
+    }
+};
+
+//NODE_CONFIG_DIR
